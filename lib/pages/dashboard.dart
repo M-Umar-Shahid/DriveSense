d0bd9aeedf64a0ff67a31d0ad4765421d4ae8764
Muhammad Umar Shahid <159911362+M-Umar-Shahid@users.noreply.github.com>
2024-12-17 23:50:27 +0500

    Bottom Navigation Bar

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -1,107 +1,148 @@
-import 'package:drivesense/pages/monitoring_screen.dart';
-import 'package:drivesense/pages/navigation_screen.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
-class DashboardPage extends StatelessWidget {
+import 'monitoring_screen.dart';
+import 'navigation_screen.dart';
+
+class DashboardPage extends StatefulWidget {
   const DashboardPage({super.key});
+
+  @override
+  State<DashboardPage> createState() => _DashboardPageState();
+}
+
+class _DashboardPageState extends State<DashboardPage> {
+  int _currentIndex = 0; // Track the selected tab
+
+  // Pages to display based on selected index
+  final List<Widget> _pages = [
+    const DashboardContent(),
+    const MonitoringPage(),
+    const MapScreen(),
+    const Center(
+      child: Text(
+        'Analytics Page',
+        style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+      ),
+    ),
+    const Center(
+      child: Text(
+        'Profile Page',
+        style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+      ),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
+        child: _pages[_currentIndex], // Dynamically load pages
+      ),
 
-              const SizedBox(height: 60.0),
-
-              // Logo and Title
-              Center(
-                child: Column(
-                  children: [
-                    CircleAvatar(
-                      radius: 50.0,
-                      backgroundColor: Colors.white,
-                      child: Image.asset(
-                        'assets/images/logo.png', // Replace with your logo image path
-                        fit: BoxFit.contain,
-                        width: 60.0,
-                        height: 60.0,
-                      ),
-                    ),
-
-                    const SizedBox(height: 10.0),
-
-                    // Title
-                    const Text(
-                      'Drive Sense',
-                      style: TextStyle(
-                        fontSize: 24.0,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-
-                    SizedBox(height: 5.0),
-
-                    // Subtitle
-                    const Text(
-                      'Access your tools for a safer drive.',
-                      style: TextStyle(
-                        fontSize: 16.0,
-                        color: Colors.grey,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-
-              SizedBox(height: 30.0),
-
-              // Buttons
-              Column(
-                children: [
-                  CircularButton(
-                    text: 'Start Recording',
-                    logoPath: 'assets/images/camera-icon.png', // Replace with your camera logo path
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => const MonitoringPage()),
-                      );
-                    },
-                  ),
-                  SizedBox(height: 20.0),
-                  CircularButton(
-                    text: 'Start Navigation',
-                    logoPath: 'assets/images/map-icon.png', // Replace with your microphone logo path
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => const MapScreen()),
-                        );
-                      },
-                  ),
-                  SizedBox(height: 20.0),
-                  CircularButton(
-                    text: 'View Analytics',
-                    logoPath: 'assets/images/analytics-icon.png', // Replace with your navigation logo path
-                    onPressed: () {},
-                  ),
-                ],
-              ),
-
-              SizedBox(height: 40.0),
-            ],
-          ),
-        ),
+      // Curved Bottom Navigation Bar
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.transparent,
+        color: Colors.blueAccent,
+        buttonBackgroundColor: Colors.blueAccent,
+        height: 60.0,
+        index: _currentIndex, // Current index
+        animationDuration: const Duration(milliseconds: 300),
+        items: const <Widget>[
+          Icon(Icons.home, size: 30, color: Colors.white),
+          Icon(Icons.camera_alt, size: 30, color: Colors.white),
+          Icon(Icons.map, size: 30, color: Colors.white),
+          Icon(Icons.analytics, size: 30, color: Colors.white),
+          Icon(Icons.person, size: 30, color: Colors.white),
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index; // Update the selected index
+          });
+        },
       ),
     );
   }
 }
 
+// This widget is the content for the "Home" tab
+class DashboardContent extends StatelessWidget {
+  const DashboardContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const SizedBox(height: 60.0),
+
+          // Logo and Title
+          Center(
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundColor: Colors.white,
+                  child: Image.asset(
+                    'assets/images/logo.png',
+                    fit: BoxFit.contain,
+                    width: 60.0,
+                    height: 60.0,
+                  ),
+                ),
+                const SizedBox(height: 10.0),
+                const Text(
+                  'Drive Sense',
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 5.0),
+                const Text(
+                  'Access your tools for a safer drive.',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 30.0),
+
+          // Buttons
+          Column(
+            children: [
+              CircularButton(
+                text: 'Start Recording',
+                logoPath: 'assets/images/camera-icon.png',
+                onPressed: () {},
+              ),
+              const SizedBox(height: 20.0),
+              CircularButton(
+                text: 'Start Navigation',
+                logoPath: 'assets/images/map-icon.png',
+                onPressed: () {},
+              ),
+              const SizedBox(height: 20.0),
+              CircularButton(
+                text: 'View Analytics',
+                logoPath: 'assets/images/analytics-icon.png',
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+// Custom Circular Button
 class CircularButton extends StatelessWidget {
   final String text;
   final String logoPath;
@@ -118,7 +159,8 @@ class CircularButton extends StatelessWidget {
     return ElevatedButton(
       onPressed: onPressed,
       style: ElevatedButton.styleFrom(
-        shape: const CircleBorder(), backgroundColor: const Color(0xFF1976D2),
+        shape: const CircleBorder(),
+        backgroundColor: const Color(0xFF1976D2),
         padding: const EdgeInsets.all(30.0),
         elevation: 5.0,
       ),
